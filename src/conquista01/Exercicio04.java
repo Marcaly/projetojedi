2e43e91edbff50e6d431a8ef084227bfb28b20ad
Marcaly <byelmarcal@hotmail.com>
2022-02-10 19:32:28 -0300

    exercicio04

---
+++ src/conquista01/Exercicio04.java
@@ -0,0 +1,23 @@
+package conquista01;
+
+        /*Criar um código que declara e inicializa variáveis do tipo primitivo dentro do método main.*/
+
+public class Exercicio04 {
+    public static void main (String[] args) {
+
+        byte b = 123;
+        /*byte = -128 a 127*/
+        short s = 32765;
+        /*short = -32768 a 32767*/
+        char  c = 64600;
+        /*char = 0 a 65636*/
+        int i = 232654;
+        /*int = –2.147.483.648 a 2.147.483.647*/
+        long l = -900000000;
+        /*long = –9.223.372.036.854.775.808 a .223.372.036.854.775.807*/
+        boolean b = true;
+        /*boolean = true or false*/
+        double d = 654564.4654;
+        float f = 131.12f;
+    }
+}
