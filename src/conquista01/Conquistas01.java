b2b6fb73f13071b41f0fd831b79c6128f5d353d2
Marcaly <byelmarcal@hotmail.com>
2022-02-10 16:28:45 -0300

    aa

---
+++ src/conquista01/Conquistas01.java
@@ -0,0 +1,16 @@
+package conquista01;
+
+        /*Declarar uma variável int numero dentro do escopo do método main e na linha seguinte atribuir um valor para ela.
+     Após isso, usar o método System.out.println() para exibir o valor da variável no console.
+      OBS*: Se atentar ao parâmetro do método de exibição
+        */
+
+public class Conquistas01 {
+    public static void main(String[] args) {
+
+        int numero;
+        numero = 69;
+        System.out.println(numero);
+
+    }
+}
