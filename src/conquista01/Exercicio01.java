1510a8bc9dfa33a67078fcdd17d854158298a97b
Marcaly <byelmarcal@hotmail.com>
2022-02-10 19:03:32 -0300

    exercicio03

---
+++ src/conquista01/Exercicio01.java
@@ -0,0 +1,16 @@
+package conquista01;
+
+  /*Declarar uma variável int numero dentro do escopo do método main e na linha seguinte atribuir um valor para ela.
+    Após isso, usar o método System.out.println() para exibir o valor da variável no console.
+    OBS*: Se atentar ao parâmetro do método de exibição
+        */
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+        int number;
+        number = 69;
+        System.out.println(number);
+
+    }
+}
---
+++ src/conquista01/Exercicio03.java
@@ -0,0 +1,19 @@
+package conquista01;
+
+    /*Criar um código dentro do método main que soma dois números e exibe o valor ao final do programa.*/
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int x,y,sum;
+        System.out.println("enter the numbers to be summed: ");
+        x=sc.nextInt();
+        y=sc.nextInt();
+        sum =  x+y;
+
+        System.out.println("the sum result is: " + sum);
+    }
+}
