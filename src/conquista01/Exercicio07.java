af5d1fa41113d160f5b9660ecbac9be898fe6e5d
Marcaly <byelmarcal@hotmail.com>
2022-02-11 19:01:53 -0300

    exc07

--- src/conquista01/Exercicio07.java
+++ src/conquista01/Exercicio07.java
@@ -3,10 +3,9 @@ package conquista01;
 public class Exercicio07 {
     public static void main (String[] args){
 
-
         System.out.println(5 != 4 && 5 == 5);
         System.out.println(91 < 7 || 9 >= 73 );
         System.out.println(83 == 83 && 4 > 7 || 9<= 3);
-        System.out.println(987 >= 988 && 2 == 2 && 9>2 ||90 < 11299);
+        System.out.println(987 >= 988 && 2 == 2 && 9>2 || 90 < 11299);
     }
 }
