06dbd3a11f5bd937fe1cd5d853d56fd8ccf3bbfc
Marcaly <byelmarcal@hotmail.com>
2022-02-11 15:38:57 -0300

    exercicio06

---
+++ src/conquista01/Exercicio06.java
@@ -0,0 +1,19 @@
+package conquista01;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+
+
+    int number1 =  7;
+    double number2 = 2913;
+    float number3 = 23.42f;
+    long number4 =  2345;
+    short number5 = 12;
+
+        System.out.println(number1 == number5);
+        System.out.println(number2 > number4);
+        System.out.println(number3 < number2);
+        System.out.println(number4 >= number5);
+        System.out.println(number5 <= number1);
+    }
+}
