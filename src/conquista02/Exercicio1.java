04a41fe9674d843289a7cf531e312ff776f3def9
Marcaly <byelmarcal@hotmail.com>
2022-02-11 21:19:19 -0300

    Exercicio-2

--- src/conquista02/Exercicio1.java
+++ src/conquista02/Exercicio1.java
@@ -1,7 +1,7 @@
 package conquista02;
 
 /*Criar um código que verifica se um valor de uma variável é igual a 100.
-  Caso verdadeiro, escrever no console: “Verdadeiro”*/
+ Caso verdadeiro, escrever no console: “Verdadeiro”*/
 
 
 import java.util.Scanner;
@@ -10,7 +10,7 @@ public class Exercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter with a number to verify");
+        System.out.println("Enter with a number to verify: ");
         int number = sc.nextInt();
 
         if (number == 100) {
---
+++ src/conquista02/Exercicio2.java
@@ -0,0 +1,22 @@
+package conquista02;
+
+/*Criar um código que verifica se um valor de uma variável é igual a cem.
+Caso verdadeiro, escrever no console: “Verdadeiro”. Caso falso, escrever no console: “Falso”
+*/
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to verify: ");
+        int number = sc.nextInt();
+
+        if (number == 100) {
+            System.out.println("Verdadeiro");
+        }else if (number != 100) {
+            System.out.println("False");
+        }
+
+    }
+}
