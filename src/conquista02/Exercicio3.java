fecd2b16777f8559821e95f58685c6d2f1a43dfb
Marcaly <byelmarcal@hotmail.com>
2022-02-11 21:53:25 -0300

    Exercicio-3

---
+++ src/conquista02/Exercicio3.java
@@ -0,0 +1,18 @@
+package conquista02;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+      int number = 78;
+
+      if (number == 100) {
+          System.out.println("True");
+      }else if (number == 23) {
+          System.out.println("True");
+      }else if (number == 2){
+          System.out.println("True");
+      }else{
+          System.out.println("False");
+      }
+    }
+}
