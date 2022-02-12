c2d4f5695181b57f3742e41bcbe765d2506e5a22
Marcaly <byelmarcal@hotmail.com>
2022-02-11 22:25:16 -0300

    Exercicio-5

---
+++ src/conquista02/Exercicio5.java
@@ -0,0 +1,24 @@
+package conquista02;
+
+/*Criar um código com uma estrutura SWITCH que verifica se o valor de uma variável é igual ao case (1, 2, 3).
+ Os cases devem escrever quando o valor for igual ao seu próprio. Mostrando apenas a frase do case referente ao valor.*/
+
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+
+        int number = 2;
+
+        switch (number) {
+            case 1:
+                System.out.println("Number 1");
+                break;
+            case 2:
+                System.out.println("Number 2");
+                break;
+            case 3:
+                System.out.println("Number 3");
+                break;
+        }
+    }
+}
