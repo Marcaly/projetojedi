5cdef6b5b96ad0735fdf8b1c926c66b0733a2018
Marcaly <byelmarcal@hotmail.com>
2022-02-16 18:04:36 -0300

    ?

---
+++ src/javaExercicios/JavaExc.java
@@ -0,0 +1,26 @@
+package javaExercicios;
+
+/*Baseado na fórmula abaixo escreva um programa que calcule e apresente a área de um círculo.
+
+A = R2
+
+Onde:
+ 	A representa a área do círculo
+  	 = 3.141592654
+	R = Raio do círculo (deve ser fornecido pelo usuário)
+*/
+
+import java.util.Scanner;
+
+public class JavaExc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+        sc.close();
+    }
+}
