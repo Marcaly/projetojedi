347b8ba5915f1c0ded6ecfe1ede242d74f2a28c9
Marcaly <byelmarcal@hotmail.com>
2022-02-17 16:25:01 -0300

    Java Exc9

---
+++ src/javaExercicios/JavaExc9.java
@@ -0,0 +1,33 @@
+package javaExercicios;
+
+/*Desenvolver um programa que apresente e calcule o volume de uma caixa retangular, utilize a fórmula abaixo:
+
+V=CLA
+
+Onde:
+	V = Volume em cm3
+	C = Comprimento
+	L = Largura
+	A = Altura
+*/
+
+import java.util.Scanner;
+
+public class JavaExc9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira a largura da caixa: ");
+        double largura = sc.nextDouble();
+        System.out.println("Insira o comprimento da caixa: ");
+        double comprimento = sc.nextDouble();
+        System.out.println("Insira a altura da caixa: ");
+        double altura = sc.nextDouble();
+
+        System.out.println("O volume da caixa retangular é =  " + comprimento * largura * altura + "cm³");
+
+
+        sc.close();
+    }
+}
