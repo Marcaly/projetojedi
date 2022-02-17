39c9ffe49993d6267c7014f5ac0e75c28dcc52b3
Marcaly <byelmarcal@hotmail.com>
2022-02-17 16:01:30 -0300

    Java Exercicio 8

---
+++ src/javaExercicios/JavaExc8.java
@@ -0,0 +1,30 @@
+package javaExercicios;
+
+/*Calcular e apresentar o valor de uma prestação em atraso, utilize a fórmula abaixo:
+
+P=VALOR + (VALOR (TAXA100)TEMPO)
+
+Onde P é o valor da prestação em atraso.*/
+
+import java.util.Scanner;
+
+public class JavaExc8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com o valor da prestação:");
+        double valor = sc.nextDouble();
+        System.out.println("Entre com a taxa de atraso da prestação: ");
+        double taxa = sc.nextDouble();
+        System.out.println("Entre com o tempo de atraso da prestação: ");
+        double tempo = sc.nextDouble();
+
+        System.out.println("O valor da prestação em atraso é = " + (valor + (valor * (taxa / 100 ) * tempo)));
+        double prestacao = valor + (valor * (taxa / 100) * tempo);
+
+
+
+        sc.close();
+    }
+}
