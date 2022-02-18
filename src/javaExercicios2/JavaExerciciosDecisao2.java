ba60a1b8d7c499d48cc98ded5793f1486aa23448
Marcaly <byelmarcal@hotmail.com>
2022-02-18 16:49:06 -0300

    Java2Exc2 100% atualizado

--- src/javaExercicios2/JavaExerciciosDecisao2.java
+++ src/javaExercicios2/JavaExerciciosDecisao2.java
@@ -3,54 +3,24 @@ package javaExercicios2;
 /*Desenvolva uma rotina que dados três números inteiros, apresente-os em ordem crescente
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class JavaExercicioDecisao2 {
+public class JavaExerciciosDecisao2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int menor = 0;
-        int meio = 0;
-        int maior = 0;
+        System.out.println("Entre com 3 números para colocar em ordem crescente");
+        int[] numeros = new int[3];
+        numeros[0] = sc.nextInt();
+        numeros[1] = sc.nextInt();
+        numeros[2] = sc.nextInt();
+        Arrays.sort(numeros);
 
-        System.out.println("Insira 3 numeros inteiros");
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int n3 = sc.nextInt();
-
-        if (n1 < n2 && n1 < n3) {
-            menor = n1;
-            System.out.println(menor);
-        } else if (n2 < n1 && n2 < n3) {
-            menor = n2;
-            System.out.println(menor);
-        } else {
-            menor = n3;
-            System.out.println(menor);
-        }
-
-        if (n1 < n2 && n1 > n3) {
-            meio = n1;
-            System.out.println(meio);
-        } else if (n2 < n1 && n2 > n3) {
-            meio = n2;
-            System.out.println(meio);
-        } else {
-            meio = n3;
-            System.out.println(meio);
-        }
-        if (n1 > n2 && n1 > n3) {
-            maior = n1;
-            System.out.println(maior);
-        } else if (n2 > n1 && n2 > n3) {
-            maior = n2;
-            System.out.println(maior);
-        } else {
-            maior = n3;
-            System.out.println(maior);
-        }
-
+        System.out.println(numeros[0]);
+        System.out.println(numeros[1]);
+        System.out.println(numeros[2]);
 
         sc.close();
     }
