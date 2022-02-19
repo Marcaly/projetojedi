10fda45a39b3d7dbdf4ef9547f3af99f82ee0ee4
Marcaly <byelmarcal@hotmail.com>
2022-02-19 19:18:47 -0300

    Exercicio03

---
+++ src/exercicios/java01/Exercicio03.java
@@ -0,0 +1,10 @@
+package exercicios.java01;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+
+        double saldo = 1360;
+        System.out.println("Saldo = " + saldo);
+        System.out.println("Saldo com reajuste = " + (saldo + saldo * 0.01));
+    }
+}
