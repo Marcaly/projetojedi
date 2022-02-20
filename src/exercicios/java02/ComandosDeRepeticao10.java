1208b613a77c20cc42f3a9ce0b8818835f0cab53
Marcaly <byelmarcal@hotmail.com>
2022-02-20 14:01:42 -0300

    Comandoss de repetição 11

---
+++ src/exercicios/java02/ComandosDeRepeticao10.java
@@ -0,0 +1,22 @@
+package exercicios.java02;
+
+/*Crie um algoritmo leia um número do usuário e exiba a sua tabuada de
+multiplicação.*/
+
+import java.util.Scanner;
+
+public class ComandosDeRepeticao10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com um número: ");
+        int numero = sc.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(numero + " * " + i + " = " + numero * i);
+        }
+
+
+        sc.close();
+    }
+}
