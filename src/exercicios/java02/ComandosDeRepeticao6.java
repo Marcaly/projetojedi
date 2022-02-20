e29c2a177eceaab5bcdcf3510bb9de80c4fffa33
Marcaly <byelmarcal@hotmail.com>
2022-02-19 22:01:30 -0300

    Comandos de repetição6

---
+++ src/exercicios/java02/ComandosDeRepeticao6.java
@@ -0,0 +1,24 @@
+package exercicios.java02;
+
+/*Leia a idade de 20 pessoas e exiba a soma das idades*/
+
+import java.util.Scanner;
+
+public class ComandosDeRepeticao6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira a idade de 20 pessoas para fazer a soma das idades: ");
+        int soma = 0;
+        int idades = 0;
+        for (int i = 0; i < 20; i++) {
+            idades = sc.nextInt();
+            soma += idades;
+        }
+        System.out.println(soma);
+
+
+        sc.close();
+    }
+}
