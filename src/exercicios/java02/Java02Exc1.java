b8dbe0fa65e021032f17f8440879a94c143ea05b
Marcaly <byelmarcal@hotmail.com>
2022-02-19 21:29:18 -0300

    Java02-Exercicio01

---
+++ src/exercicios/java02/Java02Exc1.java
@@ -0,0 +1,13 @@
+package exercicios.java02;
+
+/*Escreva um algoritmo que exiba 20 vezes a mensagem “Eu gosto de estudar
+Algoritmos!”*/
+
+public class Java02Exc1 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++){
+            System.out.println("Eu gosto de estudar algoritmos!");
+        }
+    }
+}
