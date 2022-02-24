8b2fd570c8c2417abbea585081c7ee969f169745
Marcaly <byelmarcal@hotmail.com>
2022-02-24 12:24:26 -0300

    exc4 atualizas

--- src/conquistas/conquista03/EstruturaRep04.java
+++ src/conquistas/conquista03/EstruturaRep04.java
@@ -15,7 +15,7 @@ public class EstruturaRep04 {
 
         for (int i = 1; i < 10; i++) {
             if (i % 2 == 0) {
-                System.out.println(nome);;
+                System.out.println(nome);
             }
         }
         System.out.println(nome);
