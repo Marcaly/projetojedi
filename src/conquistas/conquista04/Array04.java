ff51f222b1bd5ae5ac3ce7885449f3c1bffc6067
Marcaly <byelmarcal@hotmail.com>
2022-03-15 14:20:08 -0300

    conquista 4 done

---
+++ src/conquistas/conquista04/Array04.java
@@ -0,0 +1,31 @@
+package conquistas.conquista04;
+
+/*Criar e inicializar um array de inteiros de tamanho 10.
+ Criar também um fluxo que vai ler os dados desse array
+ e ao final dizer quantos valores são pares e quantos são ímpares*/
+
+import java.util.Scanner;
+
+public class Array04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inteiros[] = new int[10];
+        int pares = 0;
+        int impares = 0;
+
+        for (int i = 0; i < inteiros.length; i++) {
+            inteiros[i] = sc.nextInt();
+            if (inteiros[i] % 2 == 0) {
+                pares += 1;
+            } else {
+                impares += 1;
+            }
+        }
+        System.out.println("Pares: " + pares);
+        System.out.println("Ímpares: " + impares);
+
+
+        sc.close();
+    }
+}
---
+++ src/conquistas/conquista04/Array05.java
@@ -0,0 +1,45 @@
+package conquistas.conquista04;
+
+/*Criar e inicializar um array de 10 posições que vai receber 10 valores inteiros.
+Criar também um fluxo que vai ler o array e ao final exibir os seguintes dados:
+Quantos valores são maiores que 15
+Quantos valores são negativos
+Quantos valores são múltiplos de 5
+Qual a soma total dos valores
+*/
+
+import java.util.Scanner;
+
+public class Array05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inteiros[] = new int[10];
+
+        int maiores = 0;
+        int negativos = 0;
+        int multiplos = 0;
+        int soma = 0;
+
+        for (int i = 0; i < inteiros.length; i++) {
+            inteiros[i] = sc.nextInt();
+            soma += inteiros[i];
+
+            if (inteiros[i] > 15) {
+                maiores += 1;
+            } else if (inteiros[i] < 0) {
+                negativos += 1;
+            }
+            if (inteiros[i] % 5 == 0) {
+                multiplos += 1;
+            }
+        }
+        System.out.println("Números maiores que 15: " + maiores);
+        System.out.println("Números negativos: " + negativos);
+        System.out.println("Números múltiplos de 5: " + multiplos);
+        System.out.println("Soma total dos números: " + soma);
+
+
+        sc.close();
+    }
+}
