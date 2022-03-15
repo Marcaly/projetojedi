fbfc40a62b52d2f96394bc5b134f29fd6916ff90
Marcaly <byelmarcal@hotmail.com>
2022-03-15 15:22:10 -0300

    conquista 04 att

--- src/conquistas/conquista05/ForEach01.java
+++ src/conquistas/conquista05/ForEach01.java
@@ -11,20 +11,16 @@ import java.util.Scanner;
 
 public class ForEach01 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String variaveis[] = new String[10];
+        String variaveis[] = {"Casa", "Cavalo", "Produto", "Relógio", "Comida", "Buraco", "Mouse", "Trabalho", "9",
+                "10 "};
+
         String concatenar = " ";
-
-        System.out.println("Entre com 10 variáveis para concatená-las");
-
         for (String variavel : variaveis) {
-            variavel = sc.next();
             concatenar += " " + variavel;
+            System.out.println(concatenar);
         }
-        System.out.println(concatenar);
 
 
-        sc.close();
     }
 }
---
+++ src/conquistas/conquista05/ForEach02.java
@@ -0,0 +1,23 @@
+package conquistas.conquista05;
+
+/*Criar um array de tipo inteiro de 100 posições que vai receber valores de 0 a 100.
+Inicializar esse valores usando um laço de Repetição.
+E utilizando o foreach, printar todos os valores multiplicando por 2.
+*/
+
+import java.util.Scanner;
+
+public class ForEach02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inteiros[] = new int[100];
+
+
+
+
+
+
+        sc.close();
+    }
+}
